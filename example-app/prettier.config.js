2cc07e70ac5475c93c4e2255bb3cd5dbaf1039f6
Niklaas Baudet von Gersdorff <me@niklaas.eu>
2021-11-12 15:09:18 +0100

    chore: use width of 80 columns
    because better for screenshots with carbon.now.sh.

--- example-app/prettier.config.js
+++ example-app/prettier.config.js
@@ -1,16 +1,16 @@
 module.exports = {
-  printWidth: 120,
+  printWidth: 80,
   tabWidth: 2,
   useTabs: false,
   semi: true,
   singleQuote: true,
-  trailingComma: "es5",
+  trailingComma: 'es5',
   bracketSpacing: true,
   jsxBracketSameLine: false,
-  arrowParens: "avoid",
+  arrowParens: 'avoid',
   rangeStart: 0,
   rangeEnd: Infinity,
   requirePragma: false,
   insertPragma: false,
-  proseWrap: "preserve",
-}
+  proseWrap: 'preserve',
+};
